3bf52ec31dce9a0454fcfcf90f4ee79688a79b38
Gurjot Singh <55327762+GSKPROCODER@users.noreply.github.com>
2024-11-28 15:09:21 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: false,
+  experimental: {
+    appDir: true
+  }
+}
+
+module.exports = nextConfig
